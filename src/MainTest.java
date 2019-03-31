919acc4d7de07090a03eb23ddb6b4d5dab39effe
vitaly <vitaly.zubentsov@ya.ru>
2019-03-31 17:12:19 +0300

    Задание 0. Наследование

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,27 +1,20 @@
 import org.junit.Test;
 
-public class MainTest {
+public class MainTest extends CoreTestCase{
 
-    int a = 5;
-    int b = 11;
+   MathHelper Math = new MathHelper();
 
     @Test
     public void myFirstTest() {
 
-        int a = this.myltiply(5);
+        int a = Math.myltiply(5);
         System.out.println(a);
 
-        int b = this.myltiply(10,15);
+        int b = Math.myltiply(10,15);
         System.out.println(b);
     }
 
 
 
-    public int myltiply(int number) {
-        return number * 2;
-    }
 
-    public int myltiply(int number, int multiplier) {
-        return number * multiplier;
-    }
 }
---
+++ src/MathHelper.java
@@ -0,0 +1,10 @@
+public class MathHelper {
+
+    public int myltiply(int number) {
+        return number * 2;
+    }
+
+    public int myltiply(int number, int multiplier) {
+        return number * multiplier;
+    }
+}
