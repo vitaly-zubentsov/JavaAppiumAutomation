4d5a32c7560fbc0a60d6c46c3af79f4c46e4749d
vitaly <vitaly.zubentsov@ya.ru>
2019-03-31 18:10:36 +0300

    Задание 0. Super и переопределение методов

--- src/CoreTestCase.java
+++ src/CoreTestCase.java
@@ -1,2 +1,6 @@
 public class CoreTestCase {
+
+    public void typeStartMessage() {
+        System.out.println("Start test");
+    }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -2,18 +2,16 @@ import org.junit.Test;
 
 public class MainTest extends CoreTestCase {
 
-    MathHelper Math = new MathHelper();
+    public void typeStartMessage(){
+       // super.typeStartMessage();
+        System.out.println("Current class is MainTest");
+    }
+
 
     @Test
     public void myFirstTest() {
 
-        System.out.println("First test : Before changing static_int: " + MathHelper.static_int);
-
-        MathHelper.static_int = 8;
-
-        MathHelper mathObject = new MathHelper();
-        System.out.println("First test : Before changes simple_int: " + mathObject.simple_int);
-        mathObject.simple_int = 8;
+        this.typeStartMessage();
 
     }
 
@@ -21,13 +19,7 @@ public class MainTest extends CoreTestCase {
     @Test
     public void mySecondTest() {
 
-        System.out.println("Second test : Before changing static_int: " + MathHelper.static_int);
-
-        MathHelper.static_int = 8;
-
-        MathHelper mathObject = new MathHelper();
-        System.out.println("Second test : Before changes simple_int: " + mathObject.simple_int);
-        mathObject.simple_int = 8;
+        this.typeStartMessage();
 
     }
 
