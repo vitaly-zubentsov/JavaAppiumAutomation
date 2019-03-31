024a24e7ede2f187b6f0201b07c2e64c1059455d
vitaly <vitaly.zubentsov@ya.ru>
2019-03-31 16:58:22 +0300

    Задание 0. Поля и методы

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,8 +1,27 @@
 import org.junit.Test;
 
 public class MainTest {
+
+    int a = 5;
+    int b = 11;
+
     @Test
     public void myFirstTest() {
-        System.out.println("Hello World, from mac");
+
+        int a = this.myltiply(5);
+        System.out.println(a);
+
+        int b = this.myltiply(10,15);
+        System.out.println(b);
+    }
+
+
+
+    public int myltiply(int number) {
+        return number * 2;
+    }
+
+    public int myltiply(int number, int multiplier) {
+        return number * multiplier;
     }
 }
