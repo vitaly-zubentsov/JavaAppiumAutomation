cab40762029b1fdb021a8f51fa685a31f135712f
vitaly <vitaly.zubentsov@ya.ru>
2019-04-07 21:02:51 +0300

    Задание 2. Выполненно упражнение из 3го видео (wait)

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,8 +3,12 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -36,7 +40,10 @@ public class FirstTest {
 
         element_to_init_search.click();
 
-        WebElement element_to_init_search_line = driver.findElementByXPath("//*[contains(@text,'Search…')]");
+        WebElement element_to_init_search_line = waitForElementPresentByXpath("//*[contains(@text,'Search…')]", "Cannot find search input", 5);
+
+
+                //driver.findElementByXPath("//*[contains(@text,'Search…')]");
         element_to_init_search_line.sendKeys("Appium");
        // System.out.println("First test run");
     }
@@ -44,5 +51,15 @@ public class FirstTest {
     @After
     public void tearDown() {
         driver.quit();
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String eroor_message, long timeoutInSeconds) {
+
+        WebDriverWait wait = new WebDriverWait(driver,timeoutInSeconds);
+        wait.withMessage(eroor_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
     }
 }
