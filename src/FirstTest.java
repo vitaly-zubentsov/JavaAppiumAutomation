ba1a3ad18b91096d300c8a3349d561640fd01018
vitaly <vitaly.zubentsov@ya.ru>
2019-04-21 11:28:56 +0300

    Урок 3. Выполнено упражнение из 6го видео. Создан тест проверки количество результатов пустого поиска

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -318,6 +318,37 @@ public class FirstTest {
         );
     }
 
+    @Test
+    public void testAmountOfEmptySearch(){
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia'  input",
+                5
+        );
+
+        String search_line = "ads;klghet";
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                search_line,
+                "Cannot find search input",
+                5
+        );
+
+        String search_result_locator = "//*[@resource-id='org.wikipedia:id/search_results_container']//*[@resource-id='org.wikipedia:id/page_list_item_container']";
+        String empty_result_label = "//*[@text='No results found']";
+
+        waitForElementPresent(
+                By.xpath(empty_result_label),
+                "Cannot find empty result label by the request - " + search_line,
+                15
+        );
+
+        assertElementNotPresent(
+                By.xpath(search_result_locator),
+                "We've found some results by request - " + search_line
+        );
+    }
 
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
 
@@ -428,6 +459,15 @@ public class FirstTest {
 
         List elements = driver.findElements(by) ;
         return elements.size();
+    }
+
+    private void assertElementNotPresent(By by, String error_message){
+
+        int amount_of_elements = getAmountOfElements(by);
+        if (amount_of_elements > 0) {
+            String default_message = "An element " + by.toString() + " supposed to be not present";
+            throw new AssertionError(default_message + " " + error_message);
+        }
     }
 
 
