4418875536d18cf76d545cd8f2b55633963cd57e
vitaly <vitaly.zubentsov@ya.ru>
2019-04-28 17:22:15 +0300

    Урок 6. Выполнено упражнение из 1го видео. Создан отдельный класс Platform, в котором запускается драйвер iOS или Android в зависимости от переменной окружения.

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -1,31 +1,20 @@
 package lib;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class CoreTestCase extends TestCase {
 
-    private static final String
-            PLATFORM_IOS = "ios",
-            PLATFORM_ANDROID = "android";
-
     protected AppiumDriver driver;
-    private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
+    protected Platform Platform;
 
     @Override
     protected void setUp() throws Exception {
 
         super.setUp();
-
-        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
-        driver = getAppiumDriverByPlatformEnv(capabilities);
+        this.Platform = new Platform();
+        driver = this.Platform.getDriver();
         this.rotateScreenPortrait();
     }
 
@@ -48,42 +37,4 @@ public class CoreTestCase extends TestCase {
         driver.runAppInBackground(seconds);
     }
 
-    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
-
-        String platform = System.getenv("PLATFORM");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        if (platform.equals(PLATFORM_ANDROID)) {
-            capabilities.setCapability("platformName", "Android");
-            capabilities.setCapability("deviceName", "AndroidTestDevice");
-            capabilities.setCapability("platformVersion", "8.0");
-            capabilities.setCapability("automationName", "Appium");
-            capabilities.setCapability("appPackage", "org.wikipedia");
-            capabilities.setCapability("appActivity", "main.MainActivity");
-            capabilities.setCapability("app", "/Users/vitaly/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
-        } else if (platform.equals(PLATFORM_IOS)) {
-            capabilities.setCapability("platformName", "iOS");
-            capabilities.setCapability("deviceName", "iPhone SE");
-            capabilities.setCapability("platformVersion", "11.3");
-            capabilities.setCapability("app", "/Users/vitaly/Desktop/JavaAppiumAutomation/apks/Wikipedia.app");
-        } else {
-            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
-        }
-
-        return capabilities;
-    }
-
-    private AppiumDriver getAppiumDriverByPlatformEnv(DesiredCapabilities capabilities) throws MalformedURLException {
-
-        String platform = System.getenv("PLATFORM");
-
-        if (platform.equals(PLATFORM_ANDROID)) {
-            return driver = new AndroidDriver(new URL(AppiumURL), capabilities);
-        } else if (platform.equals(PLATFORM_IOS)) {
-            return driver = new IOSDriver(new URL(AppiumURL), capabilities);
-        } else {
-            throw new IllegalArgumentException("Cannot run Appuim driver by platform from env variable. Platform  value " + platform);
-        }
-
-    }
 }
--- src/tests/GetStartedTest.java
+++ src/tests/GetStartedTest.java
@@ -1,13 +1,18 @@
-package tests.iOS;
+package tests;
 
-import lib.IOSTestCase;
+import lib.CoreTestCase;
 import lib.ui.WelcomePageObject;
 import org.junit.Test;
 
-public class GetStartedTest extends IOSTestCase {
+public class GetStartedTest extends CoreTestCase {
+
 
     @Test
     public void testPassThroughWelcome(){
+
+        if (this.Platform.isAndroid()){
+            return;
+        }
 
         WelcomePageObject WelcomePage = new WelcomePageObject(driver);
 
