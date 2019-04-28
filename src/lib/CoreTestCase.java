60b47226f554c4a304009d7610753d48ce11a63c
vitaly <vitaly.zubentsov@ya.ru>
2019-04-28 12:32:15 +0300

    Урок 5. Выполнено упражнение из 5го видео. Реализован выбор опций запуска симулятора для android и ios, с помощью переменных окружения.

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -10,6 +10,10 @@ import java.net.URL;
 
 public class CoreTestCase extends TestCase {
 
+    private static final String
+            PLATFORM_IOS = "ios",
+            PLATFORM_ANDROID = "android";
+
     protected AppiumDriver driver;
     private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
 
@@ -18,16 +22,7 @@ public class CoreTestCase extends TestCase {
 
         super.setUp();
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "8.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", "main.MainActivity");
-        capabilities.setCapability("app", "/Users/vitaly/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
-
+        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
         driver = new AndroidDriver(new URL(AppiumURL), capabilities);
         this.rotateScreenPortrait();
     }
@@ -49,5 +44,30 @@ public class CoreTestCase extends TestCase {
 
     protected void backgroundApp(int seconds) {
         driver.runAppInBackground(seconds);
+    }
+
+    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
+
+        String platform = System.getenv("PLATFORM");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("deviceName", "AndroidTestDevice");
+            capabilities.setCapability("platformVersion", "8.0");
+            capabilities.setCapability("automationName", "Appium");
+            capabilities.setCapability("appPackage", "org.wikipedia");
+            capabilities.setCapability("appActivity", "main.MainActivity");
+            capabilities.setCapability("app", "/Users/vitaly/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        } else if (platform.equals(PLATFORM_IOS)) {
+            capabilities.setCapability("platformName", "iOS");
+            capabilities.setCapability("deviceName", "iPhone SE");
+            capabilities.setCapability("platformVersion", "11.3");
+            capabilities.setCapability("app", "/Users/vitaly/Desktop/JavaAppiumAutomation/apks/Wikipedia.app");
+        } else {
+            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
+        }
+
+        return capabilities;
     }
 }
